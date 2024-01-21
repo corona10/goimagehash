f6a5fdad75e5d70d7e5533a5d636856471821f53
Dong-hee Na <donghee.na92@gmail.com>
2024-01-21 22:08:57 +0900

    Address code review

--- imagehash.go
+++ imagehash.go
@@ -13,6 +13,8 @@ import (
 	"io"
 )
 
+var errNoOther = errors.New("other should not be nil")
+
 // Kind describes the kinds of hash.
 type Kind int
 
@@ -55,7 +57,7 @@ func (h *ImageHash) Bits() int {
 // Distance method returns a distance between two hashes.
 func (h *ImageHash) Distance(other *ImageHash) (int, error) {
 	if other == nil {
-		return -1, errors.New("other should not be nil")
+		return -1, errNoOther
 	}
 	if h.GetKind() != other.GetKind() {
 		return -1, errors.New("Image hashes's kind should be identical")
